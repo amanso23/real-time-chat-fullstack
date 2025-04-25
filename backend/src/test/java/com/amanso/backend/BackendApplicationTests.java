f782875292d0f620a2fc63abe83f3b7aa1c3b072
Adrian Manso <adrianmanso80@gmail.com>
2025-04-25 14:56:32 +0200

    Add Maven Wrapper and initial project setup with Spring Boot dependencies

---
+++ backend/src/test/java/com/amanso/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amanso.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
