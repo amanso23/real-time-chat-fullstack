a317244732a7c835c1aa6107f63dcec7aa91cebc
Adrian Manso <adrianmanso80@gmail.com>
2025-04-25 20:21:21 +0200

    Add missing newline at the end of application.properties

---
+++ backend/src/main/java/com/amanso/backend/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.amanso.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> req
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-resources/**",
+                                "/webjars/**",
+                                "/ws/**")
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(auth -> auth
+                        .jwt(token -> token.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter())));
+        return http.build();
+    }
+}
