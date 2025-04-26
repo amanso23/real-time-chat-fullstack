556d89fc58c39aa38d59b85420883a2a09526193
Adrian Manso <adrianmanso80@gmail.com>
2025-04-26 12:19:03 +0200

    Refactor User class to include online status logic and chat relationships

--- backend/src/main/java/com/amanso/backend/user/User.java
+++ backend/src/main/java/com/amanso/backend/user/User.java
@@ -1,11 +1,15 @@
 package com.amanso.backend.user;
 
+import java.beans.Transient;
 import java.time.LocalDateTime;
+import java.util.List;
 
+import com.amanso.backend.chat.Chat;
 import com.amanso.backend.common.BaseAuditingEntity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,11 +24,23 @@ import lombok.Setter;
 @Table(name = "users")
 public class User extends BaseAuditingEntity {
 
+    private static final int LAST_ACTIVE_INTERVAL = 5;
+
     @Id
     private Long id;
     private String firstName;
     private String lastName;
     private String email;
     private LocalDateTime lastSeen;
+
+    @OneToMany(mappedBy = "sender")
+    private List<Chat> chatsAsSender;
+    @OneToMany(mappedBy = "receiver")
+    private List<Chat> chatsAsReceiver;
+
+    @Transient
+    public boolean isOnline() {
+        return lastSeen != null && lastSeen.isAfter(LocalDateTime.now().minusMinutes(LAST_ACTIVE_INTERVAL));
+    }
 
 }
