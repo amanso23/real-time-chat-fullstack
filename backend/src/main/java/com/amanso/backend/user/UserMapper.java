f2333624bbee3aff5f26e82c309e5565500b1bea
Adrian Manso <adrianmanso80@gmail.com>
2025-04-27 19:06:56 +0200

    Refactor UserMapper class to improve code readability and maintainability

--- backend/src/main/java/com/amanso/backend/user/UserMapper.java
+++ backend/src/main/java/com/amanso/backend/user/UserMapper.java
@@ -1,25 +1,43 @@
-// package com.amanso.backend.user;
+package com.amanso.backend.user;
 
-// import java.util.Map;
+import java.time.LocalDateTime;
+import java.util.Map;
 
-// import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service;
 
-// /*
-// * Return a User object from the token attributes. The token attributes are
-// the claims
-// * returned by the identity provider (Keycloak). The User object is the entity
-// that is
-// * stored in the database.
-// */
-// @Service
-// public class UserMapper {
-// public User fromTokenAttributes(Map<String, Object> attributes) {
-// User user = new User();
+/*
+* Return a User object from the token attributes. The token attributes are the claims
+* returned by the identity provider (Keycloak). The User object is the entity that is
+* stored in the database.
+*/
+@Service
+public class UserMapper {
+    public User fromTokenAttributes(Map<String, Object> attributes) {
+        User user = new User();
 
-// if (attributes.containsKey("sub")) {
-// user.setId(Long.valueOf((String) attributes.get("sub")));
-// }
+        if (attributes.containsKey("sub")) {
+            user.setId(Long.valueOf((String) attributes.get("sub")));
+        }
 
-// }
+        if (attributes.containsKey("given_name")) {
+            user.setFirstName((String) attributes.get("given_name"));
+        } else if (attributes.containsKey("nickname")) {
+            user.setFirstName((String) attributes.get("nickname"));
+        }
 
-// }
+        if (attributes.containsKey("family_name")) {
+            user.setLastName((String) attributes.get("family_name"));
+        }
+
+        if (attributes.containsKey("email")) {
+            user.setEmail((String) attributes.get("email"));
+        }
+
+        // Set the last seen time to the current time
+        user.setLastSeen(LocalDateTime.now());
+
+        return user;
+
+    }
+
+}
