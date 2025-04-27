db675e4de26bdecc28845ed4a2e02e44c6369c6d
Adrian Manso <adrianmanso80@gmail.com>
2025-04-27 11:07:41 +0200

    Add MessageConstants class for query string constants related to messages

---
+++ backend/src/main/java/com/amanso/backend/message/MessageConstants.java
@@ -0,0 +1,13 @@
+package com.amanso.backend.message;
+
+public class MessageConstants {
+
+    public static final String FIND_MESSAGES_BY_CHAT_ID = "Messages.findMessagesByChatId";
+    public static final String SET_MESSAGES_TO_SEEN_BY_CHAT_ID = "Messages.setMessagesToSeenByChatId";
+
+    private MessageConstants() {
+        // Prevent instantiation
+
+    }
+
+}
