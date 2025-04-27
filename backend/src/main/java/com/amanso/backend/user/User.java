a8b39eb2c99b4a93ad03f8bff2bd8fb2e8980945
Adrian Manso <adrianmanso80@gmail.com>
2025-04-27 20:11:02 +0200

    Change User ID type from Long to String for consistency with database schema

--- backend/src/main/java/com/amanso/backend/user/User.java
+++ backend/src/main/java/com/amanso/backend/user/User.java
@@ -39,7 +39,7 @@ public class User extends BaseAuditingEntity {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
-    private Long id;
+    private String id;
     private String firstName;
     private String lastName;
     private String email;
