e1ac4b75a0712bdb141eabc898c303e63e41773d
Adrian Manso <adrianmanso80@gmail.com>
2025-04-27 11:22:56 +0200

    Add ChatConstants class for chat-related query string constants

---
+++ backend/src/main/java/com/amanso/backend/chat/ChatConstants.java
@@ -0,0 +1,12 @@
+package com.amanso.backend.chat;
+
+public class ChatConstants {
+
+    public static final String FIND_CHAT_BY_SENDER_ID = "Chat.findChatBySenderId";
+    public static final String FIND_CHAT_BY_SENDER_ID_AND_RECEIVER_ID = "Chat.findChatBySenderIdAndReceiverId";
+
+    private ChatConstants() {
+        // Prevent instantiation
+    }
+
+}
