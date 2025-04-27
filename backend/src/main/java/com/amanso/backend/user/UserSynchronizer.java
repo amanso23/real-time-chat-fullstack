e3e2e7f2d79ba27dc9c47f3e6ed0fd65b3b8be9b
Adrian Manso <adrianmanso80@gmail.com>
2025-04-27 19:07:04 +0200

    Enhance UserSynchronizer class with improved comments for clarity on user synchronization logic

--- backend/src/main/java/com/amanso/backend/user/UserSynchronizer.java
+++ backend/src/main/java/com/amanso/backend/user/UserSynchronizer.java
@@ -8,6 +8,12 @@ import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+/*
+ * This class is responsible for synchronizing the user with the identity provider (Keycloak).
+ * Get the claims from the given token and check if the user exists in the database by email.
+ * If the user exists call the userMapper who will return a User object from the token attributes.
+ */
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -23,6 +29,18 @@ public class UserSynchronizer {
             Optional<User> optionalUser = userRepository.findByEmail(email);
             User user = userMapper.fromTokenAttributes(token.getClaims());
 
+            // Update the user with the new attributes from the token if it exists
+            // If it doesn't exist, create a new user with the attributes from the token
+            optionalUser.ifPresentOrElse(
+                    existingUser -> {
+                        user.setId(existingUser.getId());
+                        userRepository.save(user);
+                        log.info("Updated existing user with ID: {}", existingUser.getId());
+                    },
+                    () -> {
+                        userRepository.save(user);
+                        log.info("Created new user with email: {}", email);
+                    });
         });
     }
 
