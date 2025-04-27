417cca623a3a0910dec0a76e653955cfd58d8f0e
Adrian Manso <adrianmanso80@gmail.com>
2025-04-27 20:10:39 +0200

    Add ChatMapper class for mapping Chat entities to ChatResponse DTOs

---
+++ backend/src/main/java/com/amanso/backend/chat/ChatMapper.java
@@ -0,0 +1,22 @@
+package com.amanso.backend.chat;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatMapper {
+
+    public ChatResponse toChatResponse(Chat chat, String userId) {
+        return ChatResponse.builder()
+                .id(chat.getId())
+                .name(chat.getChatName(Long.parseLong(userId)))
+                .unreadMessagesCount(chat.getUnreadMessagesCount(userId))
+                .lastMessageContent(chat.getLastMessage())
+                .lastMessageTime(chat.getLastMessageTime())
+                .isRecieverOnline(chat.getReceiver().isOnline())
+                .senderId(chat.getSender().getId())
+                .recieverId(chat.getReceiver().getId())
+                .build();
+
+    }
+
+}
