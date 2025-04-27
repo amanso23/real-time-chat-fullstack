6c395f5fdd2606437c1693c7fc6a84195f62b1f4
Adrian Manso <adrianmanso80@gmail.com>
2025-04-27 20:11:10 +0200

    Change User ID type from Long to String for consistency with the user model

--- backend/src/main/java/com/amanso/backend/user/UserRepository.java
+++ backend/src/main/java/com/amanso/backend/user/UserRepository.java
@@ -5,9 +5,12 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
 
     @Query(name = UserConstants.FIND_USER_BY_EMAIL)
     Optional<User> findByEmail(String email);
+
+    @Query(name = UserConstants.FIND_USER_BY_PUBLIC_ID)
+    Optional<User> findByPublicId(String publicId);
 
 }
