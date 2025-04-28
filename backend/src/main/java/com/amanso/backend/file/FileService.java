6669827265a84de0c699d6acbd6c4408b0ae6871
Adrian Manso <adrianmanso80@gmail.com>
2025-04-28 15:45:34 +0200

    Implement file upload functionality in FileService with directory creation and file extension handling

--- backend/src/main/java/com/amanso/backend/file/FileService.java
+++ backend/src/main/java/com/amanso/backend/file/FileService.java
@@ -1,18 +1,77 @@
 package com.amanso.backend.file;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import io.micrometer.common.lang.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import static java.io.File.separator;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.lang.System.currentTimeMillis;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class FileService {
 
-    public String saveFile(MultipartFile file, String senderId) {
-        return null;
+    @Value("${spring.application.file.uploads.media-output-path}")
+    private String fileUploadPath;
+
+    public String saveFile(@NonNull MultipartFile sourceFile, @NonNull String userId) {
+        final String fileUploadSubPath = "users" + separator + userId;
+        return uploadFile(sourceFile, fileUploadSubPath);
+    }
+
+    private String uploadFile(@NonNull MultipartFile sourceFile, @NonNull String fileUploadSubPath) {
+        final String finalUploadPath = fileUploadPath + separator + fileUploadSubPath;
+        File targetFolder = new File(finalUploadPath);
+
+        if (!targetFolder.exists()) {
+            boolean folderCreated = targetFolder.mkdirs();
+            if (!folderCreated) {
+                log.warn("Failed to create directory: {}", targetFolder.getAbsolutePath());
+                return null;
+            }
+
+        }
+
+        final String fileExtension = getFileExtension(sourceFile.getOriginalFilename());
+        String targetFiltePath = finalUploadPath + separator + currentTimeMillis() + "." + fileExtension;
+        Path targetPath = Paths.get(targetFiltePath);
+
+        try {
+            Files.write(targetPath, sourceFile.getBytes());
+            log.info("File uploaded successfully: {}", targetPath.toAbsolutePath());
+            return targetPath.toAbsolutePath().toString();
+        } catch (IOException e) {
+            log.error("Failed to upload file: {}", e.getMessage(), e);
+            return null;
+        }
+
+    }
+
+    /*
+     * Method to get the file extension from a file name.
+     */
+    private String getFileExtension(String fileName) {
+        if (fileName == null || fileName.isEmpty()) {
+            return "";
+        }
+        int lastDotIndex = fileName.lastIndexOf('.');
+        if (lastDotIndex == -1) {
+            return "";
+        }
+        return fileName.substring(lastDotIndex + 1).toLowerCase();
+
     }
 
 }
