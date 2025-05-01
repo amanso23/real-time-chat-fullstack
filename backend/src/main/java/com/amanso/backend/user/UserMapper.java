6d16c4a743a2f2eef4b20d2cb32c6f08a42256c3
Adrian Manso <adrianmanso80@gmail.com>
2025-05-01 10:46:00 +0200

    Add toUserResponse method in UserMapper to convert User to UserResponse

--- backend/src/main/java/com/amanso/backend/user/UserMapper.java
+++ backend/src/main/java/com/amanso/backend/user/UserMapper.java
@@ -40,4 +40,15 @@ public class UserMapper {
 
     }
 
+    public UserResponse toUserResponse(User user) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .lastSeen(user.getLastSeen())
+                .isOnline(user.isOnline())
+                .build();
+
+    }
 }
