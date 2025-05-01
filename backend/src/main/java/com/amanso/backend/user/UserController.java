8ef21d23a0b719d5e9e18cc112c2fa273671e95a
Adrian Manso <adrianmanso80@gmail.com>
2025-05-01 10:45:57 +0200

    Add UserController to handle user-related API requests

---
+++ backend/src/main/java/com/amanso/backend/user/UserController.java
@@ -0,0 +1,26 @@
+package com.amanso.backend.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> findAllUsersExceptSelf(Authentication authentication) {
+        return ResponseEntity.ok(userService.findAllUsersExceptSelf(authentication));
+    }
+
+}
