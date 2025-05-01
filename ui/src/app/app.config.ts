745d54eb875f3039b7adc302fd7e402dd1b6e4d2
Adrian Manso <adrianmanso80@gmail.com>
2025-05-01 16:00:25 +0200

    Refactor app configuration to include Keycloak HTTP interceptor and app initializer

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -1,8 +1,28 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { KeycloakService } from './utils/keycloak/keycloak.service';
+import { keycloakHttpInterceptor } from './utils/http/keycloak-http.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([keycloakHttpInterceptor])), // Add the created interceptor to the HTTP client
+    provideAppInitializer(() => {
+      // This function will be called when the app is initialized
+      const initFn = ((key: KeycloakService) => {
+        // This function initializes the Keycloak service
+        return () => key.init();
+      })(inject(KeycloakService));
+      return initFn(); // Call the init function to initialize Keycloak
+    }),
+  ],
 };
