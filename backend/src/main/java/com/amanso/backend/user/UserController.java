5902fc39f27d070457b503060538f70969f5a945
Adrian Manso <adrianmanso80@gmail.com>
2025-05-01 11:41:27 +0200

    Add Swagger annotations to UserController for API documentation

--- backend/src/main/java/com/amanso/backend/user/UserController.java
+++ backend/src/main/java/com/amanso/backend/user/UserController.java
@@ -3,6 +3,7 @@ package com.amanso.backend.user;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
+@Tag(name = "User", description = "User API")
 public class UserController {
 
     private final UserService userService;
