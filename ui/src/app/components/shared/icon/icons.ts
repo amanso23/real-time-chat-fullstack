70d0109b860baedf6e0410010d26b979a52ccfee
Adrian Manso <adrianmanso80@gmail.com>
2025-05-03 12:22:53 +0200

    Add icons module to export lucide-angular icons

---
+++ ui/src/app/components/shared/icon/icons.ts
@@ -0,0 +1,13 @@
+import {
+  MessageCircleIcon,
+  UserIcon,
+  DoorOpen,
+  MessageCirclePlus,
+} from 'lucide-angular';
+
+export const ICONS = {
+  MessageCircleIcon: MessageCircleIcon,
+  UserIcon: UserIcon,
+  DoorOpen: DoorOpen,
+  MessageCirclePlus: MessageCirclePlus,
+} as const;
