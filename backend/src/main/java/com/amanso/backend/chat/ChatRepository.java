64393b50b72afddee3fc48ee481749b401144e98
Adrian Manso <adrianmanso80@gmail.com>
2025-05-03 11:12:18 +0200

    Refactor ChatRepository to use UUID for chat ID and user IDs

--- backend/src/main/java/com/amanso/backend/chat/ChatRepository.java
+++ backend/src/main/java/com/amanso/backend/chat/ChatRepository.java
@@ -2,17 +2,18 @@ package com.amanso.backend.chat;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface ChatRepository extends JpaRepository<Chat, String> {
+public interface ChatRepository extends JpaRepository<Chat, UUID> {
 
     @Query(name = ChatConstants.FIND_CHAT_BY_SENDER_ID)
-    List<Chat> findChatBySenderId(@Param("senderId") String userId);
+    List<Chat> findChatBySenderId(@Param("senderId") UUID userId);
 
     @Query(name = ChatConstants.FIND_CHAT_BY_SENDER_ID_AND_RECEIVER_ID)
-    Optional<Chat> findChatBySenderIdAndReceiverId(String senderId, String receiverId);
+    Optional<Chat> findChatBySenderIdAndReceiverId(UUID senderId, UUID receiverId);
 
 }
