e1e26d93cc4ed2de2d5da2b97e9bb311713aa2e0
Adrian Manso <adrianmanso80@gmail.com>
2025-05-03 11:13:24 +0200

    Refactor UserService to use UUID for publicId instead of String

--- backend/src/main/java/com/amanso/backend/user/UserService.java
+++ backend/src/main/java/com/amanso/backend/user/UserService.java
@@ -1,6 +1,7 @@
 package com.amanso.backend.user;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -15,7 +16,7 @@ public class UserService {
     private final UserMapper userMapper;
 
     public List<UserResponse> findAllUsersExceptSelf(Authentication authentication) {
-        String publicId = authentication.getName();
+        UUID publicId = UUID.fromString(authentication.getName());
         return userRepository.findAllUsersExceptSelf(publicId)
                 .stream()
                 .map(userMapper::toUserResponse)
