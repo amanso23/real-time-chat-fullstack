e316dab4b6218f7095841195c5a27a9cb73d1290
Adrian Manso <adrianmanso80@gmail.com>
2025-05-03 11:13:18 +0200

    Refactor UserRepository to use UUID for user id instead of Long

--- backend/src/main/java/com/amanso/backend/user/UserRepository.java
+++ backend/src/main/java/com/amanso/backend/user/UserRepository.java
@@ -1,19 +1,20 @@
 package com.amanso.backend.user;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 
     @Query(name = UserConstants.FIND_USER_BY_EMAIL)
     Optional<User> findByEmail(String email);
 
     @Query(name = UserConstants.FIND_USER_BY_PUBLIC_ID)
-    Optional<User> findByPublicId(String publicId);
+    Optional<User> findByPublicId(UUID publicId);
 
     @Query(name = UserConstants.FIND_ALL_USERS_EXCEPT_SELF)
-    Optional<User> findAllUsersExceptSelf(String publicId);
+    Optional<User> findAllUsersExceptSelf(UUID publicId);
 
 }
