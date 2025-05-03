8908effc2daee27f373d090cd6a62fd1c9493045
Adrian Manso <adrianmanso80@gmail.com>
2025-05-03 11:13:41 +0200

    Add routing configuration for Main and Greet components

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { GreetComponent } from './components/greet/greet.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+  },
+  {
+    path: 'greet',
+    component: GreetComponent,
+  },
+];
