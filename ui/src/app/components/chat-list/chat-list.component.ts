4ddf3c8828395404ea4e59d90af6650610fafdcf
Adrian Manso <adrianmanso80@gmail.com>
2025-05-03 20:11:08 +0200

    Refactor ChatListComponent to include DatePipe and enhance user contact management functionality

--- ui/src/app/components/chat-list/chat-list.component.ts
+++ ui/src/app/components/chat-list/chat-list.component.ts
@@ -1,17 +1,44 @@
 import { Component, input, InputSignal } from '@angular/core';
-import { ChatResponse } from '../../services/models';
+import { ChatResponse, UserResponse } from '../../services/models';
 import { IconComponent } from '../shared/icon/icon.component';
+import { DatePipe } from '@angular/common';
+import { UserService } from '../../services/services/user.service';
 
 @Component({
   selector: 'app-chat-list',
-  imports: [IconComponent],
+  imports: [DatePipe, IconComponent],
   templateUrl: './chat-list.component.html',
   styles: '',
 })
 export class ChatListComponent {
-  searchContact() {
+  selectContact(arg0: number | undefined) {
     throw new Error('Method not implemented.');
   }
   chatList: InputSignal<ChatResponse[]> = input<ChatResponse[]>([]);
   searchNewContact: boolean = true;
+  contacts: UserResponse[] = [];
+
+  constructor(private userService: UserService) {}
+
+  wrapMessage(lastMessageContent: string) {
+    return lastMessageContent.length > 20
+      ? lastMessageContent.substring(0, 20) + '...'
+      : lastMessageContent;
+  }
+  openChat(arg0: string | undefined) {
+    throw new Error('Method not implemented.');
+  }
+  searchContact() {
+    this.userService.findAllUsersExceptSelf().subscribe({
+      next: (res) => {
+        console.log(res);
+
+        this.contacts = Array.isArray(res) ? res : res ? [res] : [];
+        this.searchNewContact = true;
+      },
+      error: (error) => {
+        console.error('Error fetching users:', error);
+      },
+    });
+  }
 }
